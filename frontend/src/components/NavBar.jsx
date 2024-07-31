9760b3be854d2200f96c413eb48aa69ec60955bf
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-31 11:05:08 +0100

    NavBar component created

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import "../styles/navbar.css";
+
+export default function NavBar() {
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <h1>AGTIA</h1>
+        </div>
+
+        <nav>
+          {/* links for : */}
+          {/* Mode button */}
+          {/* notification icon */}
+          {/* user icon */}
+        </nav>
+      </div>
+    </header>
+  );
+}
