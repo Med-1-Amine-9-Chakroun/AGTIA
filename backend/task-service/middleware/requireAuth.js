563a6a44f565544b64b2621ac79a1270bd96c2ae
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-31 10:30:13 +0100

    Adding secret key

--- backend/task-service/middleware/requireAuth.js
+++ backend/task-service/middleware/requireAuth.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 // const User = require("..")
 
@@ -6,8 +7,18 @@ const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({ error: "Authorization token required" });
-  } else {
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+
+    req.user = _id;
+
     next();
+  } catch (error) {
+    res.status(401).json({ error: "Request is not authorized" });
   }
 };
 
