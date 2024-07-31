3c091c02e2005fe0aa2e54c84957eb67b1abd6aa
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-31 11:57:04 +0100

    Login component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <Login />
     </div>
   );
 }
